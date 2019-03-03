a61064d55d942ec468a0806cd274cca886b69b24
Josh <joshdibble93@gmail.com>
2019-03-03 19:17:28 +0000

    Got it displaying dummy dates from postgres table

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import datetime
+import datefinder
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:postgres123@localhost/weight_tracker"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres123@localhost/weight_tracker"
 db = SQLAlchemy(app)
+
 
 class Data(db.Model):
     __tablename__ = "data"
-    id = db.Column(db.Integer, primary_key = True)
-    date_ = db.Column(db.Date, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    date_ = db.Column(db.Date, unique=True)
     stone_ = db.Column(db.Integer)
     pounds_ = db.Column(db.Integer)
 
@@ -18,18 +22,26 @@ class Data(db.Model):
         self.stone_ = stone_
         self.pounds_ = pounds_
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/results")
 def results():
-    rows=[]
-    dates = db.session.query(Data).all()
-    for d in dates:
-        rows.append(d)
-    print(rows)
-    return render_template("results.html", rows=rows)
+    ##
+    dates = db.session.query(Data.date_).all()
+    result = [str(i) for i in dates]
+    ls = []
+    for i in result:
+        matches = datefinder.find_dates(i)
+        for match in matches:
+            match = match.date()
+            match = match.strftime("%d/%m/%Y")
+            ls.append(match)
+    print(ls)
+    return render_template("results.html", dates=ls)
 
 
 @app.route("/success", methods=["POST"])
@@ -40,12 +52,11 @@ def success():
         pounds = request.form["lb_entry"]
         # print(date)
         if db.session.query(Data).filter(Data.date_ == date).count() == 0:
-            data=Data(date, stone, pounds)
+            data = Data(date, stone, pounds)
             db.session.add(data)
             db.session.commit()
             return render_template("success.html")
         return render_template("index.html", text="You've already got an entry for that date")
-
 
 
 if __name__ == "__main__":
